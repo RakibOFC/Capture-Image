c8438731799b232ed605235e36afade9117c345c
RakibOFC <rakibull2000@gmail.com>
2022-10-02 11:37:25 +0600

    Customize alarm boardcast receiver

--- app/src/main/java/com/rakibofc/captureimage/Alarm.java
+++ app/src/main/java/com/rakibofc/captureimage/Alarm.java
@@ -8,8 +8,12 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.os.Build;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 
 public class Alarm extends BroadcastReceiver {
@@ -21,24 +25,26 @@ public class Alarm extends BroadcastReceiver {
         Toast.makeText(context, extra, Toast.LENGTH_SHORT).show();
 
         addNotification(context, "Notification");
+
+        // pushNotification(context, "Prayer Time Alert", "contentTitle", "contentText", 11111);
     }
 
     private void addNotification(Context context, String channelId) {
 
-        NotificationChannel channel = null;
+        NotificationChannel notificationChannel;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            channel = new NotificationChannel(
+            notificationChannel = new NotificationChannel(
                     channelId,
                     channelId,
-                    NotificationManager.IMPORTANCE_LOW
+                    NotificationManager.IMPORTANCE_HIGH // IMPORTANCE_LOW
             );
-            channel.setShowBadge(true);
-            context.getSystemService(NotificationManager.class).createNotificationChannel(channel);
+            notificationChannel.setShowBadge(true);
+            context.getSystemService(NotificationManager.class).createNotificationChannel(notificationChannel);
         }
 
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(context, channelId)
-                        .setSmallIcon(R.drawable.ic_launcher_background) //set icon for notification
+                        .setSmallIcon(R.mipmap.ic_launcher) //set icon for notification
                         .setContentTitle("Notifications Example") //set title of notification
                         .setContentText("This is a notification message")//this is notification message
                         .setAutoCancel(true) // makes auto cancel of notification
@@ -51,10 +57,39 @@ public class Alarm extends BroadcastReceiver {
         notificationIntent.putExtra("message", "This is a notification message");
 
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        builder.setDefaults(Notification.DEFAULT_SOUND);
         builder.setContentIntent(pendingIntent);
 
         // Add as notification
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         manager.notify(0, builder.build());
+    }
+
+    public Notification pushNotification(Context context, String channelId, String title, String message, int requestCode) {
+
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    channelId,
+                    channelId,
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setDescription("This is prayer reminder notification");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
+
+        Intent intent = new Intent(context, MainActivity.class);
+        @SuppressLint("InlinedApi") PendingIntent pendingIntent = PendingIntent.getActivity(context, requestCode, intent, PendingIntent.FLAG_IMMUTABLE);
+        mBuilder.setContentIntent(pendingIntent);
+        mNotificationManager.notify(requestCode, mBuilder.build());
+
+        return mBuilder.build();
     }
 }
